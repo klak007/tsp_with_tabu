2d072c41303d4c11fb5d6d303cc6b14cef29995f
klak007 <aleksander.kl@outlook.com>
2024-04-22 01:00:56 +0200

    change of the generation of initial solution, change of checking neighbors, added aspiration criteria - increasing/ decreasing tabu size. changed color palette of plots inside analyze.py. now grouped by max tabu size in main.py  changed dataframe columns from tabusize to min and max tabu size.
    tspdata3 is the one to compare without any todays improvements, tspdata4 is with todays improvements but withour 2-opt neighborhood, tspdata5 is with every improvement.
    
    same goes with plot directories

--- tabu_search.py
+++ tabu_search.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 def distance(city1, city2):
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
@@ -14,37 +16,49 @@ def total_distance(route, cities):
     return total
 
 
-def generate_initial_solution(num_cities):
-    return np.random.permutation(num_cities)
+def generate_initial_solution(cities):
+    num_cities = len(cities)
+    current_city = 0  # Start from the first city
+    solution = [current_city]
+
+    unvisited_cities = set(range(1, num_cities))  # All cities except the first one are unvisited
+
+    while unvisited_cities:
+        next_city = min(unvisited_cities, key=lambda city: distance(cities[current_city], cities[city]))
+        unvisited_cities.remove(next_city)
+        solution.append(next_city)
+        current_city = next_city
+
+    return solution
 
 
-def get_neighbors(solution):
+def get_neighbors(solution):  # 2-opt neighborhood
     neighbors = []
     for i in range(len(solution)):
-        for j in range(i + 1, len(solution)):
+        for j in range(i + 2, len(solution) + (i > 0)):
             neighbor = solution.copy()
-            neighbor[i], neighbor[j] = neighbor[j], neighbor[i]
+            neighbor[i:(j % len(solution))] = reversed(solution[i:(j % len(solution))])
             neighbors.append(neighbor)
     return neighbors
 
 
-def tabu_search(cities, max_iterations, tabu_size):
-    num_cities = len(cities)
-    current_solution = generate_initial_solution(num_cities)
+def tabu_search(cities, max_iterations, min_tabu_size, max_tabu_size):
+    current_solution = generate_initial_solution(cities)
     best_solution = current_solution.copy()
     best_distance = total_distance(best_solution, cities)
     tabu_list = []
+    tabu_size = min_tabu_size
 
-    for _ in range(max_iterations):
+    for iteration in range(max_iterations):
         neighbors = get_neighbors(current_solution)
         best_neighbor = None
         best_neighbor_distance = float('inf')
 
         for neighbor in neighbors:
-            if tuple(neighbor) in tabu_list:
+            neighbor_distance = total_distance(neighbor, cities)
+            if tuple(neighbor) in tabu_list and neighbor_distance >= best_distance:
                 continue
 
-            neighbor_distance = total_distance(neighbor, cities)
             if neighbor_distance < best_neighbor_distance:
                 best_neighbor = neighbor
                 best_neighbor_distance = neighbor_distance
@@ -60,8 +74,14 @@ def tabu_search(cities, max_iterations, tabu_size):
         if best_neighbor_distance < best_distance:
             best_solution = best_neighbor
             best_distance = best_neighbor_distance
+            # aspiration criteria decrease tabu size when we find a better solution
+            # and increase tabu size when we don't find a better solution
+            tabu_size = max(min_tabu_size, tabu_size - 1)
+        else:
+            tabu_size = min(max_tabu_size, tabu_size + 1)
 
     return best_solution, best_distance
+
 
 def route_plot(cities, best_route, max_iterations, tabu_size):
     # Create a new figure
@@ -78,9 +98,9 @@ def route_plot(cities, best_route, max_iterations, tabu_size):
         city2 = cities[best_route[i + 1]]
         plt.plot([city1[0], city2[0]], [city1[1], city2[1]], 'r')
 
-    #title
+    # title
     plt.title(f'Tabu Search: {len(cities)} cities, {max_iterations} iterations, {tabu_size} tabu size')
     # Save the plot in the route_plots directory
-    plt.savefig(f'route_plots2/tsp_plt_cit{len(cities)}_iter{max_iterations}_ts{tabu_size}.png')
+    plt.savefig(f'route_plots5/tsp_plt_cit{len(cities)}_iter{max_iterations}_ts{tabu_size}.png')
     # Display the plot
     # plt.show()
